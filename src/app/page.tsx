540e35de771425f92c8feb912d9e4ebf792d8794
Janne <radius@hotmail.fi>
2026-08-25 20:09:35 +0300

    feat: julkaistu uusi Aura B2B Tech -laskeutumissivu tiedottajanne.com

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,5 @@
-import Hero from "@/components/Hero";
-import ProofOfWork from "@/components/ProofOfWork";
-import LatestVideosAeo from "@/components/LatestVideosAeo";
-import ServicesAndTraining from "@/components/ServicesAndTraining";
-import ChannelReferences from "@/components/ChannelReferences";
-import AiPromptLibrary from "@/components/AiPromptLibrary";
-import SoteRoiCalculator from "@/components/SoteRoiCalculator";
-import SoteGlossary from "@/components/SoteGlossary";
-import ConsultingBookingSection from "@/components/ConsultingBookingSection";
+import AuraLanding from "@/components/AuraLanding";
 
 export default function Home() {
-  return (
-    <div>
-      <Hero />
-      <ProofOfWork />
-      <LatestVideosAeo />
-      <ServicesAndTraining />
-      <ChannelReferences />
-      <AiPromptLibrary />
-      <SoteRoiCalculator />
-      <SoteGlossary />
-      <ConsultingBookingSection />
-    </div>
-  );
+  return <AuraLanding />;
 }
