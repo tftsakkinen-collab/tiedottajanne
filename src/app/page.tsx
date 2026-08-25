a1ae174580a3abc71b861aed29ef4dfe3e4892ab
Janne <radius@hotmail.fi>
2026-08-25 20:32:41 +0300

    Revert "feat: julkaistu uusi Aura B2B Tech -laskeutumissivu tiedottajanne.com"
    This reverts commit 540e35de771425f92c8feb912d9e4ebf792d8794.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
-import AuraLanding from "@/components/AuraLanding";
+import Hero from "@/components/Hero";
+import ProofOfWork from "@/components/ProofOfWork";
+import LatestVideosAeo from "@/components/LatestVideosAeo";
+import ServicesAndTraining from "@/components/ServicesAndTraining";
+import ChannelReferences from "@/components/ChannelReferences";
+import AiPromptLibrary from "@/components/AiPromptLibrary";
+import SoteRoiCalculator from "@/components/SoteRoiCalculator";
+import SoteGlossary from "@/components/SoteGlossary";
+import ConsultingBookingSection from "@/components/ConsultingBookingSection";
 
 export default function Home() {
-  return <AuraLanding />;
+  return (
+    <div>
+      <Hero />
+      <ProofOfWork />
+      <LatestVideosAeo />
+      <ServicesAndTraining />
+      <ChannelReferences />
+      <AiPromptLibrary />
+      <SoteRoiCalculator />
+      <SoteGlossary />
+      <ConsultingBookingSection />
+    </div>
+  );
 }
